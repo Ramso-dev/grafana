fcf531326e129fa5ae1b2a73b58095a6971def12
Ramso-dev <rossijatime@gmail.com>
2017-05-10 11:42:01 +0200

    first

--- run.sh
+++ run.sh
@@ -10,7 +10,6 @@ fi
 source /etc/sysconfig/grafana-server
 cd /usr/share/grafana
 /usr/sbin/grafana-server \
---config=${CONF_FILE} \
 --pidfile=${PID_FILE} \
 cfg:default.paths.logs=${LOG_DIR} \
 cfg:default.paths.data=${DATA_DIR} \
